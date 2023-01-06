8d2df698dc19ba7858c99f663e1769033ffcfe82
DavidELacy <121587070+DavidELacy@users.noreply.github.com>
2023-01-05 20:06:48 -0500

    Fix Setup Script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@ git remote add deploy ssh://lacyfamily@lacy.family:21098/home/lacyfamily/david.l
 git remote add all git@github.com:DavidELacy/testrepo.git
 git remote set-url --add --push all git@github.com:DavidELacy/testrepo.git
 git remote set-url --add --push all ssh://lacyfamily@lacy.family:21098/home/lacyfamily/david.lacy.family/testrepo
+git fetch --all
 git branch -u all/master
